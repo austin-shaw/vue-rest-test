019545a1494d1e40d92661f0b5d1052399655bae
Austin Shaw <austinshaw_@hotmail.com>
2019-04-25 15:59:21 -0500

    Updated Title and Github link

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <v-app>
     <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vue</span>
-        <span class="font-weight-light"> TEST REST</span>
+      <v-toolbar-title class="headline">
+        <span>vue-rest-test</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
         icon
-        href="https://github.com/austin-shaw"
+        href="https://github.com/austin-shaw/vue-rest-test"
         target="_blank"
       >
         <v-icon>fab fa-github</v-icon>

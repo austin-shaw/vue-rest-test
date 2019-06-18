d02d5275286fcb0d7373c15fe499ef5eb5762ba1
Austin Shaw <austinshaw_@hotmail.com>
2019-06-18 14:24:04 -0500

    Updated theming

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <v-app>
     <v-toolbar app>
+      <v-img :src="require('./assets/st-logo.png')"
+        max-height=50px
+        max-width=50px
+       ></v-img>
       <v-toolbar-title class="headline">
-        <span>Rest API Test</span>
+        <span>Smoke Test</span>
       </v-toolbar-title>
+      <v-spacer/>
+    <v-toolbar-items>
+    </v-toolbar-items>
       <v-spacer/>
       <v-btn
         icon
         href="https://github.com/austin-shaw/vue-rest-test"
         target="_blank"
+        color="secondary"
       >
         <v-icon>fab fa-github</v-icon>
       </v-btn>
     </v-toolbar>
-
     <v-content>
       <Home/>
     </v-content>
@@ -35,3 +42,9 @@ export default {
   }
 }
 </script>
+
+<style>
+.v-toolbar__title{
+  color:#435466
+}
+</style>
